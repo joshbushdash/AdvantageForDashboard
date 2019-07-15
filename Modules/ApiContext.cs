754032740a2d666e3e11b8579f5cdafaa3e3dafd
workPC <zihao.zheng@autodesk.com>
2019-07-15 11:17:37 +0800

    problem fixed 3

--- Modules/ApiContext.cs
+++ Modules/ApiContext.cs
@@ -7,7 +7,7 @@ namespace Advantage.API.Models
         public ApiContext(DbContextOptions<ApiContext> options) : base(options) { }
 
         public DbSet<Customer> Customers { get; set; }
-        public DbSet<Order> COrders { get; set; }
+        public DbSet<Order> Orders { get; set; }
         public DbSet<Server> Servers { get; set; }
     }
 }
